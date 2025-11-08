3bd285b17c3b6a9698ec7d92b104d45e3b58bc3d
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-11-08 22:10:03 +0530

    started implementing jwt

--- src/database.py
+++ src/database.py
@@ -10,30 +10,30 @@ post_table = sqlalchemy.Table(
     "posts",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.String, primary_key=True),
-    sqlalchemy.Column("body", sqlalchemy.String)
+    sqlalchemy.Column("body", sqlalchemy.String),
+)
+
+
+user_table = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("email", sqlalchemy.String, unique=True),
+    sqlalchemy.Column("password", sqlalchemy.String),
 )
 
 
 comment_table = sqlalchemy.Table(
     "comments",
     metadata,
-    sqlalchemy.Column("id",
-                      sqlalchemy.String,
-                      primary_key=True
-                      ),
-    sqlalchemy.Column("body",
-                      sqlalchemy.String
-                      ),
-    sqlalchemy.Column("post_id",
-                      sqlalchemy.ForeignKey("posts.id"),
-                      nullable=False
-                      )
+    sqlalchemy.Column("id", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False),
 )
 
 
 engine = sqlalchemy.create_engine(
-    config.DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    config.DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 
@@ -41,6 +41,5 @@ metadata.create_all(engine)
 
 
 database = databases.Database(
-    config.DATABASE_URL,
-    force_rollback=config.DB_FORCE_ROLL_BACK
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
 )
