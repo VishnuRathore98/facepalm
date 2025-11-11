6956567a761fea3cbc36ce87d22f03008b74dee4
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-11-11 07:44:09 +0530

    added test for user endpoint

---
+++ src/tests/routers/test_user.py
@@ -0,0 +1,31 @@
+import pytest
+from httpx import AsyncClient
+
+
+async def register_user(async_client: AsyncClient, email: str, password: str):
+    return await async_client.post(
+        "/register",
+        json={
+            "email": email,
+            "password": password,
+        },
+    )
+
+
+@pytest.mark.anyio
+async def test_register_user(async_client: AsyncClient):
+    response = await register_user(async_client, "test@mail.com", "test123")
+    assert response.status_code == 201
+
+
+@pytest.mark.anyio
+async def test_already_registered_user(
+    async_client: AsyncClient,
+    registered_user: dict,
+):
+    response = await register_user(
+        async_client,
+        registered_user["email"],
+        registered_user["password"],
+    )
+    assert response.status_code == 400
---
+++ test_user.py
@@ -0,0 +1,29 @@
+   facepalm
+ ├╴  src
+ │ ├╴  models
+ │ ├╴  routers
+ │ ├╴  tests
+ │ │ ├╴  routers
+ │ │ │ ├╴  __init__.py
+ │ │ │ ├╴  test_comments.py
+ │ │ │ ├╴  test_posts.py
+ │ │ │ └╴  test_security.py
+ │ │ ├╴  __init__.py
+ │ │ ├╴  conftest.py
+ │ │ └╴  test_sample.py
+ │ ├╴  __init__.py
+ │ ├╴  config.py
+ │ ├╴  database.py
+ │ ├╴  logging_conf.py
+ │ ├╴  main.py
+ │ ├╴  security.py
+ │ └╴  test.db
+ ├╴  README.md
+ ├╴  __init__.py
+ ├╴  data.db
+ ├╴  facepalm.log
+ ├╴  pyproject.toml
+ ├╴  pyrightconfig.json
+ ├╴  test.db
+ └╴  uv.lock
+
